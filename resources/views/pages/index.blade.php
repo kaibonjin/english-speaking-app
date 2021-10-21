e088091aaefc6182bf8a0d8abb4d5b61de6633a9
kaibonjin <kaibonjin@gmail.com>
2021-10-21 12:26:33 +0900

    add カウンセリング予約ボタン

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.layout')
 @section('content')
-    トップページでる。
+<a href="{{ url('/reservation')}}" class="text-sm text-gray-700 dark:text-gray-500 underline">無料カウンセリング！</a>
 @endsection
